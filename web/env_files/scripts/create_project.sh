7e7d7de4122231bd7449b6a97adc8be70ff222a9
MashioKamiya <kmymso.wk@gmail.com>
2020-09-18 23:31:58 +0900

    [delete] docker-compose.yml
    環境構築用ファイルを全て破棄

--- web/env_files/scripts/create_project.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd /
-composer create-project --prefer-dist laravel/laravel app "6.*"
-chmod -R 777 /app/storage
--- web/env_files/scripts/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-rm -rf /app/composer.lock
-composer install
-
-cp /app/.env.example /app/.env
-php /app/artisan key:generate
