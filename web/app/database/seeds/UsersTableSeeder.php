07a5851be52199ec35b7b06a5b37f2d7537a2c79
Haruna Nagata <haruna@HarunanoMacBook-Pro.local>
2020-09-28 11:47:37 +0900

    seeder

--- web/app/database/seeds/UsersTableSeeder.php
+++ web/app/database/seeds/UsersTableSeeder.php
@@ -11,31 +11,29 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        \DB::table('posts')->insert([
-            'id' => '1',
-            'user_id' => '1',
-            'photo' => 'ここは写真',
-            'spot' => '××公園',
-            'area_id' => '5',
-            'access' => '京都府',
-            'comment' => '綺麗でした。',
-            'created_at' => date('Y-m-d H:i:s'),
-            'updated_at' => date('Y-m-d H:i:s'),
+        // \DB::table('posts')->insert([
+        //     'user_id' => '1',
+        //     'photo' => 'https://img-cdn.guide.travel.co.jp/article/999/31206/6E27665A71E34BA2B52CDB816ECBB6EE_LL.jpg',
+        //     'spot' => '××公園',
+        //     'area_id' => '5',
+        //     'access' => '京都府',
+        //     'comment' => '綺麗でした。',
+        //     'created_at' => date('Y-m-d H:i:s'),
+        //     'updated_at' => date('Y-m-d H:i:s'),
 
-        ]);
+        // ]);
 
-        \DB::table('posts')->insert([
-            'id' => '2',
-            'user_id' => '2',
-            'photo' => 'ここは写真',
-            'spot' => '〇〇海辺',
-            'area_id' => '9',
-            'access' => '沖縄',
-            'comment' => 'おすすめスポット！',
-            'created_at' => date('Y-m-d H:i:s'),
-            'updated_at' => date('Y-m-d H:i:s'),
+        // \DB::table('posts')->insert([
+        //     'user_id' => '2',
+        //     'photo' => 'test2.jpg',
+        //     'spot' => '〇〇海',
+        //     'area_id' => '9',
+        //     'access' => '沖縄',
+        //     'comment' => 'おすすめスポット！',
+        //     'created_at' => date('Y-m-d H:i:s'),
+        //     'updated_at' => date('Y-m-d H:i:s'),
 
-        ]);
+        // ]);
 
     }
 }
