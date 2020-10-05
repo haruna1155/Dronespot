3cc21820ccac02ce572a183c6564d8e7dcf60fbe
Haruna Nagata <haruna@HarunanoMacBook-Pro.local>
2020-10-05 22:32:27 +0900

    細かな修正

--- web/app/resources/views/commons/favorite_button.blade.php
+++ web/app/resources/views/commons/favorite_button.blade.php
@@ -1,13 +1,13 @@
 @if (Auth::id() != $post->id)
-    @if (Auth::user() && Auth::user()->is_favorites($post->id))
-        {{--unfavorite--}}
-        {!! Form::open(['route' => ['unfavorite', $post->id], 'method' => 'delete']) !!}
-            {!! Form::button('<i class="fas fa-star"></i>', ['class' => "btn btn-outline-warning btn-sm", 'type' => 'submit']) !!}
-        {!! Form::close() !!}
-    @else
+    @if ($spot->favorite->isEmpty())
         {{-- favorite--}}
         {!! Form::open(['route' => ['favorite', $post->id]]) !!}
             {!! Form::button('<i class="far fa-star"></i>', ['class' => "btn btn-outline-warning btn-sm", 'type' => 'submit']) !!}
+        {!! Form::close() !!}
+    @else
+        {{--unfavorite--}}
+        {!! Form::open(['route' => ['unfavorite', $post->id], 'method' => 'delete']) !!}
+            {!! Form::button('<i class="fas fa-star"></i>', ['class' => "btn btn-outline-warning btn-sm", 'type' => 'submit']) !!}
         {!! Form::close() !!}
     @endif
 @endif
